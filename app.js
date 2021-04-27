d5a4b27144c1e4dba5bbce466b91a9a8e822a45a
MorsteoJS <48878487+Morsteo-JS@users.noreply.github.com>
2021-04-27 11:17:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ Voice.on("voiceStateUpdate", async(oldState, newState) => {
     }
 });
 
-Voice.login("Token").catch(err => {
+Voice.login(lozayar.TOKEN).catch(err => {
     Voice.error("Ses istemcisine bağlanırken bir hata oluştu: " + err.message);
     return Voice.destroy();
 });
